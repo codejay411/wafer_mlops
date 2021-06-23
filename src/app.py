2c7aac377327445d15b1e244b5615c02de666007
codejay411 <jaypr202@gmail.com>
2021-06-23 18:38:52 +0530

    update requirements

--- src/app.py
+++ src/app.py
@@ -3,10 +3,10 @@ from flask import Flask, request, render_template
 from flask import Response
 import os
 from flask_cors import CORS, cross_origin
-from prediction_Validation_Insertion import pred_validation
-from trainingModel import trainModel
 from training_Validation_Insertion import train_validation
+from trainingModel import trainModel
 import flask_monitoringdashboard as dashboard
+from prediction_Validation_Insertion import pred_validation
 from predictFromModel import prediction
 import json
 
